2e6ced224d72163d1890ce15fe87ebc4a987def9
dan <dan.abramov@gmail.com>
2024-02-24 20:04:46 +0000

    Use a single global infinite promise (#34)
    It's not super clear why the code is written this way, but I assume it's due to not being familiar with the pattern. If you want to wait until the `freeze` prop changes, it's enough to throw a Promise that never resolves.
    
    This fixes an issue that would occur on initial mount with `createRoot` and/or Fabric. The issue is that React throws away uncommitted trees if they suspend during mount, and this includes throwing away `useRef`. So stashing Promises in `useRef` is wrong. Having one at the top level is both safe and simpler.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import React, { useRef, Suspense, Fragment } from "react";
+import React, { Suspense, Fragment } from "react";
 
-interface StorageRef {
-  promise?: Promise<void>;
-  resolve?: (value: void | PromiseLike<void>) => void;
-}
+const infiniteThenable = { then() {} };
 
 function Suspender({
   freeze,
@@ -12,19 +9,9 @@ function Suspender({
   freeze: boolean;
   children: React.ReactNode;
 }) {
-  const promiseCache = useRef<StorageRef>({}).current;
-  if (freeze && !promiseCache.promise) {
-    promiseCache.promise = new Promise((resolve) => {
-      promiseCache.resolve = resolve;
-    });
-    throw promiseCache.promise;
-  } else if (freeze) {
-    throw promiseCache.promise;
-  } else if (promiseCache.promise) {
-    promiseCache.resolve!();
-    promiseCache.promise = undefined;
+  if (freeze) {
+    throw infiniteThenable;
   }
-
   return <Fragment>{children}</Fragment>;
 }
 
